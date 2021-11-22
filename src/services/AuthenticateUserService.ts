5c5b5d2d74caf71e45db34d86298135320eb3c6a
sandro.umekita <sandro.umekita@acct.global>
2021-11-22 00:45:20 -0300

    Criação do repositório backend, configuração das dependencias, integração com o github

---
+++ src/services/AuthenticateUserService.ts
@@ -0,0 +1,47 @@
+import axios from "axios";
+import { response } from "express";
+
+interface IAcessTokenResponse {
+  access_token: string;
+}
+
+interface IUserResponse {
+  avatar_url: string;
+  login: string;
+  id: number;
+  name: string;
+}
+
+class AuthenticateUserService {
+  async execute(code: string) {
+    const url = "http://github.com/login/oauth/access_token";
+
+    const { data: accessTokenResponse } = await axios.post<IAcessTokenResponse>(
+      url,
+      null,
+      {
+        params: {
+          client_id: process.env.GITHUB_CLIENT_ID,
+          client_secret: process.env.GITHUB_CLIENT_SECRET,
+          code,
+        },
+        headers: {
+          Accept: "application/json",
+        },
+      }
+    );
+
+    const response = await axios.get<IUserResponse>(
+      "http://api.github.com/user",
+      {
+        headers: {
+          authorization: `Bearer ${accessTokenResponse.access_token}`,
+        },
+      }
+    );
+
+    return response.data;
+  }
+}
+
+export { AuthenticateUserService };
